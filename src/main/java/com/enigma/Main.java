4e3c871e79f52b1f5f3419eee5948209ee5f092f
Lucas Paiva <lpaivamatiazzi@gmail.com>
2024-06-08 10:44:06 -0300

    feat: add main menu message

--- src/main/java/com/enigma/Main.java
+++ src/main/java/com/enigma/Main.java
@@ -1,7 +1,22 @@
 package com.enigma;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
 
+public class Main {
+
+    public static void main(String[] args) {
+        printMainMenu();
+    }
+
+    private static void printMainMenu() {
+        Path menuTextPath = Path.of("src/main/resources/introduction.txt");
+        try (Stream<String> menuText = Files.lines(menuTextPath)) {
+            menuText.forEach(System.out::println);
+        } catch (IOException e) {
+            System.err.format("%s%n", e);
+        }
     }
 }
